39023b03acdfdea982fe5c1a2c8ac4ba0484709c
ivancp54 <113448066+ivancp54@users.noreply.github.com>
2023-01-19 20:43:53 +0100

    Hay grafico pero no bien
    astic fart

--- ej10main.py
+++ ej10main.py
@@ -1,25 +1,15 @@
 import matplotlib.pyplot as plt
-from ejercicio10 import *
+import ejercicio10 as ej10
 
 def main():
     # Obtener los datos de las funciones
-    dato1 = clock_speed()
-    dato2 = pixels()
-    dato3 = bateria()
+    dato1 = ej10.clock_speed()
+    dato1.plot.bar()
+
+    #dato2 = ej10.pixels()
+    #dato3 = ej10.bateria()
     # Crear el gráfico de barras
-    plt.bar(range(len(dato1)), dato1, label="clock_speed")
-    plt.bar(range(len(dato2)), dato2, label="pixels")
-    plt.bar(range(len(dato3)), dato3, label="bateria")
-
-    # Establecer las opciones del gráfico
-    plt.xlabel("Data")
-    plt.ylabel("Value")
-    plt.title("Graph")
-    plt.legend()
-
-    # Mostrar el gráfico
     plt.show()
+    return dato1
 
-
-if __name__ == "__main__":
-    main()
+print(main())
--- ejercicio10.py
+++ ejercicio10.py
@@ -4,21 +4,27 @@ import pandas as pd
 df = pd.read_csv("test.csv")
 # IDs a seleccionar
 ids = [3, 13, 34, 56, 70, 85, 110, 120, 210, 400]
+
 # seleccionar las filas con esos ids
 df_sel = df.loc[df["id"].isin(ids)]
+
 # imprimir los datos de las filas seleccionadas
 print(df_sel)
+
 def clock_speed():
-    df_clock_speed = df_sel["clock_speed"]
-    print(df_clock_speed)
+    df_clock_speed = df_sel[["id","clock_speed"]]
+    return df_clock_speed
 
 def pixels():
-    df_px = df_sel[["px_height","px_width"]]
+    df_px = df_sel[["id","px_height","px_width"]]
     print(df_px)
+    return df_px
 
 def bateria():
-    df_bat = df_sel["battery_power"]
+    df_bat = df_sel[["id","battery_power"]]
     print(df_bat)
+    return df_bat
 
-
-print(bateria())
+clock_speed()
+pixels()
+bateria()
